2d5949f92aad235b11f1dc09a4e20880d897659b
Sam Lalli <2sam.lalli@gmail.com>
2020-08-14 10:34:59 -0600

    task 1 and 2 finished

--- index.js
+++ index.js
@@ -208,11 +208,18 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[3].name);
+console.log(artists[2].bio);
+
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+function changeName(arr, index, name){
+  arr[index].name = name
+  return arr;
+}
+console.log(changeName(artists, 8, "Vinvent Van Gogh"));
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
